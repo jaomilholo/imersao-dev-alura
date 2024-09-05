576618c1a13244c9adc9d25a7a9f65d87a1059e5
jaomilholo <joaomilholo12@gmail.com>
2024-09-04 21:12:31 -0300

    first commit

---
+++ assets/js/dados.js
@@ -0,0 +1,33 @@
+let profissoesWow = [
+  {
+    title: 'Esfolamento',
+    description: 'Coleta de couro, escamas e pelegos.',
+    type: 'coleta',
+    img: 'esfolamento.jpg'
+  },
+  {
+    title: 'Herborismo',
+    description: 'Coleta de ervas e plantas.',
+    type: 'coleta',
+    img: 'herborismo.jpg'
+  },
+  {
+    title: 'Mineração',
+    description: 'Coleta de minérios e pedras preciosas.',
+    type: 'coleta',
+    img: 'mineracao.jpg'
+  },
+  {
+    title: 'Alfaiataria',
+    description: 'Criação de roupas e tecidos.',
+    type: 'produção',
+    img: 'alfaiataria.jpg'
+  },
+  // ... Adicione os demais objetos para as outras profissões
+  {
+    title: 'Engenharia',
+    description: 'Criação de gadgets e máquinas.',
+    type: 'produção',
+    img: 'engenharia.jpg'
+  }
+];
